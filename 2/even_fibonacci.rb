dc7bdf92ff70dbd01aac6effb69df344a3e97183
Matt Jenkins <matt@modeblue.co.uk>
2013-12-03 11:43:05 -0600

    add comments

--- 2/even_fibonacci.rb
+++ 2/even_fibonacci.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/env ruby
 
 def fibsum(n)
-  #generate fib numbers
-  fiblist = [1,1] 
   result = 0
 
+  #generate fib numbers
+  fiblist = [1,1] 
   fiblist << (fiblist[-2] + fiblist[-1])   while (fiblist[-2]+fiblist[-1]) <= n 
 
+  # For each number in the list, add to result if it is even.
   fiblist.each { |x| result+=x if x%2==0 }
+
   result
 end 
 
