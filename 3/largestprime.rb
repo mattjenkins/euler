98df4a609e54b4332966fb3fed12aca02ccc111c
Matt Jenkins <matt@modeblue.co.uk>
2013-12-03 21:21:24 -0600

    level3 complete.

---
+++ 3/largestprime.rb
@@ -0,0 +1,22 @@
+def largestprime(n)
+  # calculate primes
+  def isprime(x)
+    # count up to the sqrt of n (rounded up) and if any of the numbers can be divded with no remainder, return false. 
+    # (excluding 1 and itself)
+    2.upto(Math.sqrt(x).round) {|y|  return false if x%y == 0}
+    # if no numbers divides, its a prime.  return true.
+    return true
+  end
+  
+  # find biggest prime under n
+  # count down from n-1 (ignore itself) and as soon as a prime is found, return it
+  (n-1).downto(2)  do |j|  
+    #puts "n =" + n.to_s + ",   j =" + j.to_s
+    return j if isprime(j)
+  end    
+  # if no prime is found, return false.
+  return false
+end
+
+
+
---
+++ 3/largestprime_spec.rb
@@ -0,0 +1,25 @@
+require "largestprime"
+
+# "What is the largest prime factor of the number 600851475143 ?"
+
+describe "Calculate the largest prime" do
+  it "when X=10" do
+    result = largestprime(10)
+    result.should == 7
+  end
+  
+  it "when X=30" do
+    result = largestprime(30)
+    result.should == 29
+  end
+end
+
+describe "Calculate the largest Prime for Project Euler #3" do
+  it "when X=600851475143" do
+    result = largestprime(600851475143)
+    puts "The answer is " + result.to_s 
+  end
+  
+end
+  
+  
