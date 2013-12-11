844053a94c635973bd1ebcc7118cbc884c0aa04b
Matt Jenkins <matt@modeblue.co.uk>
2013-12-10 19:50:33 -0600

    Added check for factor.  Changed the iteration method

--- 3/largestprime.rb
+++ 3/largestprime.rb
@@ -1,24 +1,25 @@
 #!/usr/bin/env ruby 
 
-def largestprime(n)
   # calculate primes
-  def isprime(x)
-    # count up to the sqrt of n (rounded up) and if any of the numbers can be divded with no remainder, return false. 
-    # (excluding 1 and itself)
-    2.upto(Math.sqrt(x).round) {|y|  return false if x%y == 0}
-    # if no numbers divides, its a prime.  return true.
-    return true
-  end
+def isprime(x)
+  # count up to the sqrt of n (rounded up) and if any of the numbers can be divded with no remainder, return false. 
+  # (excluding 1 and itself)
+  2.upto(Math.sqrt(x).round) {|y|  return false if x%y == 0}
+  # if no numbers divide, its a prime.  return true.
+  return true
+end
   
-  # find biggest prime under n
-  # count down from n-1 (ignore itself) and as soon as a prime is found, return it
-  (n-1).downto(2)  do |j|  
-    #puts "n =" + n.to_s + ",   j =" + j.to_s
-    return j if isprime(j)
-  end    
+def factor(x, factor)
+    return (x%factor==0)
+end
+  
+
+def largestprime(n)
+  i, testvalue = 0,0
+  2.upto(n/2) do |i|
+    testvalue = n/i  # also need to not check the same testvalue twice
+    return testvalue if isprime(testvalue) && factor(n,testvalue)
+  end
   # if no prime is found, return false.
   return false
 end
-
-
-
--- 3/largestprime_spec.rb
+++ 3/largestprime_spec.rb
@@ -5,13 +5,22 @@ require "largestprime"
 describe "Calculate the largest prime" do
   it "when X=10" do
     result = largestprime(10)
-    result.should == 7
+    result.should == 5
   end
   
   it "when X=30" do
     result = largestprime(30)
+    result.should == 5
+  end
+  it "when X=4087" do
+    result = largestprime(4087)
+    result.should == 67
+  end
+  it "when X=13195" do
+    result = largestprime(13195)
     result.should == 29
   end
+  
 end
 
 describe "Calculate the largest Prime for Project Euler #3" do
