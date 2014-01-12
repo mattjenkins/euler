ea873eb4ce6226114d253351030d99c592f1c1f7
Matt Jenkins <matt@modeblue.co.uk>
2014-01-11 23:14:39 -0600

    add comment

--- 25/findfib.rb
+++ 25/findfib.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/env ruby 
 def findfib(n=nil)
   fibseq=[1,1]
+
+  #generate fib sequence as long as n
   until fibseq[-1].to_s.length==n
     fibseq<<fibseq[-2]+fibseq[-1]
   end
+
   return fibseq.length
 end
